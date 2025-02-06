6dc097434f9664338fbebd26d84efab2228f3691
Erre131 <50458923+Erre131@users.noreply.github.com>
2025-02-06 12:28:32 +0100

    Add files via upload

---
+++ fakeAI.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+    localStorage.clear();
+}
+
+//Funzione per andare nella pagina indicata nell'url dopo aver "usato l'AI per individuare qualcosa" 
+// (quindi dopo che l'AI simulata ha finito di parlare dopo tot msec)
+// function infoFound(url,msec){
+//     setTimeout(
+//         ()=>{window.location=url},
+//         msec
+//     );
+// }
+
+function startAI(audiofile, url, msec){
+    var audio= new Audio(audiofile);
+    audio.play();
+    setTimeout(
+        ()=>{window.location=url},
+        msec
+    );
+}
+
