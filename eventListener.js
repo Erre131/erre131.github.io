b2b268a7583d353a742b89071fe9b556a67a37d0
Erre131 <50458923+Erre131@users.noreply.github.com>
2025-04-08 15:47:31 +0200

    Add files via upload

--- eventListener.js
+++ eventListener.js
@@ -30,32 +30,50 @@ btns.forEach(button =>
           });
   });
 
+//eventListener, se selezioni quantity da aggiungere di prodotto aggiunto
+//manda a pagina prod aggiunto con quantità selezionata
+let addUnitsForm = document.getElementById('addUnitsForm');
+
+addUnitsForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let quantAdd = document.getElementById('add-quantity');
+
+  if (quantAdd.value == "" ) {
+    alert("Errore nel valore inserito!");
+  } else {
+    // vai su pagina productadded è aggiornala con scritto "hai aggiunto tot unità di questo"
+    prodAdd = document.getElementById('quant-aggiunta');
+    prodAdd.innerHTML = quantAdd.value + " unità di"
+    quantAdd.value="";
+  }
+});
+
   
 //Definisce la struttura della finestra di notifica per prodotto in scadenza
-$( function() {
-    $( "#exp-notification" ).dialog({
-      resizable: false,
-      height: "auto",
-      width: 400,
-      modal: true,
-      buttons: {
-        "Visualizza prodotto": function() {
-          $( this ).dialog( "close" );
-          //salva il prodotto come selectedProduct nel localStorage
-          let prodcliccato = document.getElementById('exp-product-name').innerHTML;
-          localStorage.setItem('selectedProduct', prodcliccato);
-          //va nella pagina coi dati del prodotto
-          window.location = 'productdata.html';
-        },
-        "Lista prodotti in scadenza": function(){
-          $( this ).dialog( "close" );
-          window.location = 'expiredproductlist.html';
-        },
-        Chiudi: function() {
-          $( this ).dialog( "close" );
-          window.history.go(-1);
-        }
-      }
-    });
-  } );
-  
+//MESSO IN EXPNOTIFICATION sennò non lo prendeva
+// $( function() {
+//     $( "#exp-notification" ).dialog({
+//       resizable: false,
+//       height: "auto",
+//       width: 400,
+//       modal: true,
+//       buttons: {
+//         "Visualizza prodotto": function() {
+//           $( this ).dialog( "close" );
+//           //salva il prodotto come selectedProduct nel localStorage
+//           let prodcliccato = document.getElementById('exp-product-name').innerHTML;
+//           localStorage.setItem('selectedProduct', prodcliccato);
+//           //va nella pagina coi dati del prodotto
+//           window.location = 'productdata.html';
+//         },
+//         "Lista prodotti in scadenza": function(){
+//           $( this ).dialog( "close" );
+//           window.location = 'expiredproductlist.html';
+//         },
+//         Chiudi: function() {
+//           $( this ).dialog( "close" );
+//           window.history.go(-1);
+//         }
+//       }
+//     });
+//   } );
